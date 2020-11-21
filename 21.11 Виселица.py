2b4adab15e2fe826a41fd2125106e97129dfe3e1
Ekaterina Divina <ekaterinadivina@gmail.com>
2020-11-21 18:11:55 +0300

    Update 21.11 Виселица.py

--- 21.11 Виселица.py
+++ 21.11 Виселица.py
@@ -1,14 +1,20 @@
 from random import randint # Добавляем библиотеку случайных чисел
 
+words  = ["австралия", "индия", "джибути", "англия", "филиппины"]
 
+random_index = randint(0, 4)
 
-word = "австралия"
+word = words[random_index]
+print(word)
+game = True #Игра повторяется, пока эта переменная True
+lives = 10 #В начале игры у игрока 10 жизней
 
-game = True
 
 while game:
     print()
-    
+    print()
+
+    print("*"+"___*"*len(word))
     letter = input("Введите букву или слово: ")
 
     if letter == word:
@@ -20,3 +26,13 @@ while game:
         
     else:
         print("Не подходит")
+        lives = lives - 1
+    print("Осталось", lives, "жизней")
+
+    #Если жизни кончились, цикл должен остановиться
+    if lives == 0:
+        print("У тебя кончились жизни, ты проиграл")
+        game = False
+
+    
+print("Спасибо за игру, приходи ещё!")
