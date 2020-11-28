465a7c6581a00b36a26432005c63aa36a092c83c
Ekaterina Divina <ekaterinadivina@gmail.com>
2020-11-28 16:48:16 +0300

    Update 21.11 Виселица.py

--- 21.11 Виселица.py
+++ 21.11 Виселица.py
@@ -1,19 +1,22 @@
 from random import randint # Добавляем библиотеку случайных чисел
 
-words  = ["австралия", "индия", "джибути", "англия", "филиппины"]
 
-random_index = randint(0, 4)
 
-word = words[random_index]
+words  = ["австралия", "индия", "джибути", "англия", "филиппины"] #список слов
+
+random_index = randint(0, 4) #генерируем число от 0 до 4 
+word = words[random_index] #записываем в переменную word слово из списка wordS со случайным индексом
+
 print(word)
+
 game = True #Игра повторяется, пока эта переменная True
 lives = 10 #В начале игры у игрока 10 жизней
 
+letters = []#список угаданных букв
 
 while game:
     print()
     print()
-
     print("*"+"___*"*len(word))
     letter = input("Введите букву или слово: ")
 
@@ -23,11 +26,13 @@ while game:
         
     elif letter in word:
         print("Есть такая буква!")
+        letters.append(letter) #Добавляем в список угаданных букв 
         
     else:
         print("Не подходит")
         lives = lives - 1
     print("Осталось", lives, "жизней")
+    print("Угаданные буквы: ", letters)
 
     #Если жизни кончились, цикл должен остановиться
     if lives == 0:
