fc749d4131f8e3adf15aa49bbffeba091dc09786
Ekaterina Divina <ekaterinadivina@gmail.com>
2020-12-05 18:17:54 +0300

    Update 05_12_2020 Игра 21.py

--- 05_12_2020 Игра 21.py
+++ 05_12_2020 Игра 21.py
@@ -1,12 +1,23 @@
-
+from random import randint
 
 
 
 
 #####
 print("добро пожаловать в игру 21")
-n = int(input("Сколько игроков будет?\t"))
+n = int(input("Сколько игроков будет?\t"))  
 print()
 
-for i in range(n):
+players = ["rtrte", "werw", "wrgwegw"]
+
+for i in range(n):  #НОВЫЙ ЦИКЛ FOR
     name = input("Введите имя " +str(i+1)+"-го игрока:\t" )
+    ##HW Добавить имена игроков в список
+
+
+points = [] #Сколько очков набрал каждый игрок
+for j in range(n):
+    random_points = randint(1, 10)
+    ##HW Записать рандомное число в список points
+    print(players[j], "Ваш счёт:", random_points)
+    
