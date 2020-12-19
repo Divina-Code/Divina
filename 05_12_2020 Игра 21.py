0bdac60d5db29e5093da94bebea8715abee17afa
Ekaterina Divina <ekaterinadivina@gmail.com>
2020-12-19 18:02:44 +0300

    Update 05_12_2020 Игра 21.py

--- 05_12_2020 Игра 21.py
+++ 05_12_2020 Игра 21.py
@@ -18,33 +18,51 @@ print("\nИГРОКИ: ", players)
 
 ### Генерируем каждому игроку его стартовый счёт
 points = [] #Сколько очков набрал каждый игрок
-for j in range(n):
+for i in range(n):
     random_points = randint(1, 10)
     points.append(random_points)
-    print(players[j], "Ваш счёт:", random_points)
+    print(players[i], "Ваш счёт:", random_points)
 
 print("\nОЧКИ: ",points)
 
+
+###Продолжает игрок или нет
+playORnot = []
+for i in range(n):
+    playORnot.append(True)
+    #Добавилли True столько раз, сколько у нас игроков
+
 ### ОСНОВНАЯ ИГРА НАЧИНАЕТСЯ ТУТ
-game = True #Игра продолжается или закончена
-while game:
+
+while playORnot.count(True) >0:
     print("\n \t\t\tROUND")
 
-    for a in range(n):
-        answer = input(players[a] + ' будете брать карту? [ДА\НЕТ]')
+    for i in range(n):
+        if playORnot[i] == True:
+            answer = input(players[i] + ' будете брать карту? [ДА\НЕТ]')
 
-        answer = answer.upper() #Делаем все буквы ЗАГЛАВНЫМИ
-        answer = answer.strip()  #УБираем лишние пробелы, если они есть
+            answer = answer.upper() #Делаем все буквы ЗАГЛАВНЫМИ
+            answer = answer.strip()  #УБираем лишние пробелы, если они есть
 
-        if answer == "ДА":
-            print("Вы ответили ", answer)
-            ##!!!!! Сгенерировать новое число от 1 до 10 и прибавить к счёту игрока
-            ## Вывести игроку его новый счёт
-        elif answer == "НЕТ":
-            print("Вы ответили ", answer)
-        else:
-            print("Не понял твоего ответа, принимаю только 'ДА' или 'НЕТ' ")
+            if answer == "ДА":
+                print("Вы ответили ", answer)
+                random_points = randint(1, 10)
+                points[i]+=random_points
+                print(players[i], "Вам выпало:", random_points)
+
+                if points[i] >=21:
+                    playORnot[i] = False
+                    ###Если игрок набрал 21 очко или больше, то он выбывает из игры
+
+
+                ##!!!!! Сгенерировать новое число от 1 до 10 и прибавить к счёту игрока
+                ## Вывести игроку его новый счёт
+            elif answer == "НЕТ":
+                print("Вы ответили ", answer)
+                playORnot[i] = False
+            else:
+                print("Не понял твоего ответа, принимаю только 'ДА' или 'НЕТ' ")
 
     print("\n____________________________")
-    for b in range(n): # Выводим счёт для каждого игрока
-        print(players[b], "У вас очков:", points[b])
+    for i in range(n): # Выводим счёт для каждого игрока
+        print(players[i], "У вас очков:", points[i])
