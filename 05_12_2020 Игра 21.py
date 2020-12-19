13e6c7ec2f2395a8a120550318cbbda4a90dcee2
Ekaterina Divina <ekaterinadivina@gmail.com>
2020-12-19 16:36:09 +0300

    Update 05_12_2020 Игра 21.py

--- 05_12_2020 Игра 21.py
+++ 05_12_2020 Игра 21.py
@@ -7,14 +7,16 @@ print("добро пожаловать в игру 21")
 n = int(input("Сколько игроков будет?\t"))  
 print()
 
-
+###Записываем имена игроков в список
 players = [] #Список имён игроков
 for i in range(n):  #НОВЫЙ ЦИКЛ FOR
     name = input("Введите имя " +str(i+1)+"-го игрока:\t" )
-    players.append(name)
+    players.append(name) #Добавляем имя в список
 
 print("\nИГРОКИ: ", players)
 
+
+### Генерируем каждому игроку его стартовый счёт
 points = [] #Сколько очков набрал каждый игрок
 for j in range(n):
     random_points = randint(1, 10)
@@ -23,8 +25,11 @@ for j in range(n):
 
 print("\nОЧКИ: ",points)
 
+### ОСНОВНАЯ ИГРА НАЧИНАЕТСЯ ТУТ
 game = True #Игра продолжается или закончена
 while game:
+    print("\n \t\t\tROUND")
+
     for a in range(n):
         answer = input(players[a] + ' будете брать карту? [ДА\НЕТ]')
 
